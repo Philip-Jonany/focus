1dda9587343e9bb52ffaf575697cef9a63323c2f
Philip-Jonany <philip.jonani@gmail.com>
2023-10-05 00:36:06 -0700

    fixed bug where time doesn't go to initial time after reaching 0

--- yuh.js
+++ yuh.js
@@ -1,10 +1,10 @@
 const timerContainer = document.querySelector(".timerContainer"); 
-
+const initialTime = 25 * 60;
 // time remaining in seconds
-var time = 25 * 60;
+var time = initialTime
 var startAudio = new Audio('start.mp3')
-var doneAudio = new Audio('done.mp3');
-var intervalID;
+var doneAudio = new Audio('done.mp3')
+var intervalID
 
 function start () {
     intervalID = setInterval(updateTimer,1000);
@@ -16,16 +16,17 @@ function pause() {
 }
 
 function updateTimer() {
-    if (time <= 0 ) {
-        doneAudio.play()
-        setTimeout(function(){doneAudio.play()}, 2000)
-        clearInterval(intervalID);
-    }
-
     const seconds = time % 60;
     const minutes = Math.floor(time / 60) % 60;
     document.querySelector(".seconds").innerText = seconds;
     document.querySelector(".minutes").innerText = minutes;
     document.title = minutes + ":" + seconds;
     time -=1;
+
+    if (time <= 0 ) {
+        doneAudio.play()
+        setTimeout(function(){doneAudio.play()}, 2000)
+        clearInterval(intervalID)
+        time = initialTime;
+    }
 }
